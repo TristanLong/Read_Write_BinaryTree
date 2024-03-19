998c0c5efe4ba9d2c2cff72a05cb4fb88198ebe9
TristanLong <trieukimlong102000@gmail.com>
2024-03-19 21:05:10 +0700

    by TristanLong

---
+++ Read_Write_Tree/Read_Write_Binary_Tree.cpp
@@ -0,0 +1,68 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+// Định nghĩa cấu trúc của một nút trong cây nhị phân
+struct TreeNode {
+    int data;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int value) : data(value), left(nullptr), right(nullptr) {}
+};
+
+// Hàm để chèn một giá trị vào cây nhị phân
+void insertIntoBST(TreeNode*& root, int value) {
+    if (root == nullptr) {
+        root = new TreeNode(value);
+    }
+    else {
+        if (value < root->data) 
+            insertIntoBST(root->left, value);
+        else 
+            insertIntoBST(root->right, value);
+    }
+}
+
+// Hàm để đọc dữ liệu từ tệp và xây dựng cây nhị phân
+void readBinaryTreeFromFile(TreeNode*& root, ifstream& inFile) {
+    if (!inFile.is_open()) {
+        cout << "Không thể mở tệp " << endl;
+        return;
+    }
+
+    //Biểu thức inFile >> value sẽ trả về giá trị true nếu việc đọc thành công, và false nếu đọc đã kết thúc hoặc xảy ra lỗi.
+    int value;
+    while (inFile >> value) {
+        insertIntoBST(root, value);
+    }
+
+    inFile.close();
+}
+
+// Hàm để ghi dữ liệu của cây nhị phân vào tệp
+void writeBinaryTree(TreeNode* root, ofstream& outFile) {
+    if (root) {
+        outFile << root->data << " "; // Ghi dữ liệu của nút hiện tại
+        writeBinaryTree(root->left, outFile); // Ghi cây con trái
+        writeBinaryTree(root->right, outFile); // Ghi cây con phải
+    }
+}
+
+int main() {
+    // Tạo cây nhị phân từ dữ liệu trong tệp văn bản
+    TreeNode* root = nullptr;
+    ifstream inFile("input.txt");
+    readBinaryTreeFromFile(root, inFile);
+
+    // Ghi cây nhị phân vào tệp
+    ofstream outFile("BinaryTree.txt");
+    writeBinaryTree(root, outFile);
+    outFile.close();
+
+    // Giải phóng bộ nhớ của cây nhị phân
+    delete root;
+
+    return 0;
+}
---
+++ Read_Write_Tree/Read_Write_Matrix.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct maTrix {
+    int n;
+    int list[100][100];
+};
+
+void read(maTrix& m) {
+    ifstream infile("Text.txt");
+    if (!infile.is_open()) {
+        cout << "Không thể mở tệp!" << endl;
+        return;
+    }
+
+    infile >> m.n;
+    for (int i = 0; i < m.n; i++) {
+        for (int j = 0; j < m.n; j++) {
+            infile >> m.list[i][j];
+        }
+    }
+
+    infile.close(); // Đóng tệp sau khi đọc xong.
+}
+
+void write(maTrix m) {
+    ofstream outfile("Output.txt");
+    if (!outfile.is_open()) {
+        cout << "Không thể mở tệp đầu ra!" << endl;
+        return;
+    }
+
+    outfile << m.n << endl;
+    for (int i = 0; i < m.n; i++) {
+        for (int j = 0; j < m.n; j++) {
+            outfile << m.list[i][j] << " ";
+        }
+        outfile << endl;
+    }
+
+    outfile.close(); // Đóng tệp sau khi ghi xong.
+}
+
+//int main() {
+//    maTrix myMatrix;
+//
+//    // Đọc dữ liệu từ tệp "Text.txt"
+//    read(myMatrix);
+//
+//    // In dữ liệu ra màn hình
+//    cout << "Ma trận doc duoc:" << endl;
+//    for (int i = 0; i < myMatrix.n; i++) {
+//        for (int j = 0; j < myMatrix.n; j++) {
+//            cout << myMatrix.list[i][j] << " ";
+//        }
+//        cout << endl;
+//    }
+//
+//    // Ghi dữ liệu vào tệp "Output.txt"
+//    write(myMatrix);
+//
+//    return 0;
+//}
